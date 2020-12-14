66586e65c572759617615c83f4bfbfc36af6bcdf
KBL <kibinlee19k@gmail.com>
2020-12-14 14:54:52 +0900

    first entity restaurant

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 
 @Module({
   imports: [
@@ -32,8 +33,9 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       "username": process.env.DB_USERNAME,
       "password": process.env.DB_PASSWORD,
       "database": process.env.DB_NAME,
-      "synchronize": true,
+      "synchronize": process.env.NODE_ENV !== "prod",
       "logging": true,
+      entities: [Restaurant],
     }),
     RestaurantsModule,
   ],
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,22 +1,31 @@
 import { Field, ObjectType } from "@nestjs/graphql";
-import { IsBoolean, IsString, Length } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+
+    @PrimaryGeneratedColumn()
+    @Field(type=>Number)
+    id: number
+
     @Field(type=>String)
-    @IsString()
-    @Length(5, 10)
+    @Column()
     name:string;
 
     @Field(type=>Boolean)
-    @IsBoolean()
+    @Column()
     isGood?: boolean;
     
     @Field(type=>String)
-    @IsString()
+    @Column()
     address:string;
 
     @Field(type=>String)
-    @IsString()
+    @Column()
     ownerName:string;
+
+    @Field(type =>String)
+    @Column()
+    categoryName: string;
 }
