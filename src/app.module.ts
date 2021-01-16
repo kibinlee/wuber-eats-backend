2c53c3a8d9a237d261b135974eee4d1470aa8177
KBL <kibinlee19k@gmail.com>
2021-01-16 19:14:44 +0900

    AuthGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { CommonModule } from './common/common.module';
 import { User } from './users/entities/user.entity';
 import { JwtModule } from './jwt/jwt.module';
 import { JwtMiddleware } from './jwt/jwt.middleware';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -54,7 +55,8 @@ import { JwtMiddleware } from './jwt/jwt.middleware';
       privateKey: process.env.PRIVATE_KEY,
     }),
     UsersModule,
-    CommonModule,
+    // CommonModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,6 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
+import { AuthGuard } from 'src/auth/auth.guard';
 import {
   CreateAccountInput,
   CreateAccountOutput,
@@ -49,6 +51,7 @@ export class UserResolver {
   }
 
   @Query((returns) => User)
+  @UseGuards(AuthGuard)
   me(@Context() context) {
     console.log(context);
   }
