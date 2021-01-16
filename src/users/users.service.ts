2c8f27f3383063571bedc5f2eae09ca046f7ad82
KBL <kibinlee19k@gmail.com>
2021-01-16 21:46:46 +0900

    updateProfile part Three

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -70,7 +70,18 @@ export class UserService {
     return this.users.findOne({ id });
   }
 
-  async editProfile(userId: number, { email, password }: EditProfileInput) {
-    return this.users.update(userId, { email, password });
+  async editProfile(
+    userId: number,
+    { email, password }: EditProfileInput,
+  ): Promise<User> {
+    // console.log(editProfileInput);
+    const user = await this.users.findOne(userId);
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+    return this.users.save(user);
   }
 }
