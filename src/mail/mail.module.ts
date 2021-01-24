1867930512e01841e6c79fc54c6fee0d4395e996
KBL <kibinlee19k@gmail.com>
2021-01-24 23:38:51 +0900

    Mail Module Setup

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,19 @@
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Module({})
+@Global()
+export class MailModule {
+  static forRoot(options: MailModuleOptions): DynamicModule {
+    return {
+      module: MailModule,
+      providers: [
+        {
+          provide: CONFIG_OPTIONS,
+          useValue: options,
+        },
+      ],
+    };
+  }
+}
