3bca4aeae941b9bb3cf659e6a98fda2b1b410dde
KBL <kibinlee19k@gmail.com>
2021-01-25 22:34:33 +0900

    Setting Up Tests and Mocking

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,48 @@
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
+import { UserService } from './users.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
+
+describe('UserService', () => {
+  let service: UserService;
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        UserService,
+        { provide: getRepositoryToken(User), useValue: mockRepository },
+        { provide: getRepositoryToken(Verification), useValue: mockRepository },
+        { provide: JwtService, useValue: mockJwtService },
+        { provide: MailService, useValue: mockMailService },
+      ],
+    }).compile();
+    service = module.get<UserService>(UserService);
+  });
+
+  it('be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it.todo('createAccount');
+  it.todo('login');
+  it.todo('findById');
+  it.todo('editProfile');
+  it.todo('verifyEmail');
+});
