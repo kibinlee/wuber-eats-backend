da76e30a1b139dc7bbf27e35628158e23707d73c
KBL <kibinlee19k@gmail.com>
2021-01-26 22:42:14 +0900

    Testing createAccount part One

--- test/users.app.e2e-spec.ts
+++ test/users.app.e2e-spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+const GRAPHQL_ENDPOINT = '/graphql';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -11,7 +13,6 @@ describe('UserModule (e2e)', () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
     app = module.createNestApplication();
     await app.init();
   });
@@ -21,10 +22,38 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
-  it.todo('me');
+  describe('createAccount', () => {
+    const EMAIL = 'nico@las.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it.todo('should fail if account already exists');
+  });
+
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
+  it.todo('editProfile');
 });
