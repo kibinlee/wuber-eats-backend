9aa715c39e27e1ff05a89363b5994e2a78c057e2
KBL <kibinlee19k@gmail.com>
2021-01-28 22:47:31 +0900

    Roles part Tow

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,17 +1,32 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
+import { User } from 'src/users/entities/user.entity';
+import { AllowedRoles } from './role.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
   canActivate(context: ExecutionContext) {
+    const roles = this.reflector.get<AllowedRoles>(
+      'roles',
+      context.getHandler(),
+    );
+    if (!roles) {
+      return true;
+    }
+
     //We take the ExecutionContext into GQL
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    const user = gqlContext['user'];
+    const user: User = gqlContext['user'];
     if (!user) {
       //block the request
       return false;
     }
     //let the request continue
-    return true;
+    if (roles.includes('Any')) {
+      return true;
+    }
+    return roles.includes(user.role);
   }
 }
