f1d639a3ebd11799ba2125ea56f8ac7d3862104e
KBL <kibinlee19k@gmail.com>
2021-02-07 00:58:13 +0900

    change server port to 4000

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { JwtModule } from './jwt/jwt.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
