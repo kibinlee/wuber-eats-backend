9092b6d988c52fe7ef82f62c3df261a19b3aa879
KBL <kibinlee19k@gmail.com>
2021-02-12 19:49:34 +0900

    Promoting Restaurants part Two

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -3,7 +3,7 @@ import { Cron, Interval, SchedulerRegistry, Timeout } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Repository } from 'typeorm';
+import { LessThan, Repository } from 'typeorm';
 import {
   CreatePaymentInput,
   CreatePaymentOuput,
@@ -72,6 +72,20 @@ export class PaymentService {
         error: 'Could not load payments.',
       };
     }
+  }
+
+  @Interval(2000)
+  async checkPromotedRestaurants() {
+    const restaurants = await this.restaurants.find({
+      isPromoted: true,
+      promotedUntil: LessThan(new Date()),
+    });
+    console.log(restaurants);
+    restaurants.forEach(async (restaurant) => {
+      restaurant.isPromoted = false;
+      restaurant.promotedUntil = null;
+      await this.restaurants.save(restaurant);
+    });
   }
   //   @Cron('30 * * * * *', {
   //     name: 'myJob',
